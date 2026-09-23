ae28919e5664d2e18b087a28e7f85088cf441b2e
shahzaibnadeem123 <hafizshahg2442@gmail.com>
2026-09-23 04:53:21 -0700

    Day 08 React Vite Setup

---
+++ day-08-react-vite/src/components/main.jsx
@@ -0,0 +1,12 @@
+function Main() {
+  return (
+    <main>
+      <h2>Welcome to My React App</h2>
+      <p>
+        This is the main content of my React Vite application.
+      </p>
+    </main>
+  );
+}
+
+export default Main;
